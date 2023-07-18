1c02faa7fc27f6658cd29e95317f8eaf1bb86044
Dmitry K <okdas@pm.me>
2023-07-18 15:25:40 -0700

    add healthcheck

---
+++ src/pages/api/health.ts
@@ -0,0 +1,8 @@
+
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+    res.status(200).send('OK');
+};
+
+export default handler;
